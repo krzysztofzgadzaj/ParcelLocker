9ebcc91c2b2f10fefc3e2a633a65da09a4e0343a
Krzysztof Zgadzaj <kzgadzaj@future-processing.com>
2023-06-04 16:25:46 +0200

    Fronted solution initialized.

---
+++ src/navigation/routerConfig.js
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+
+export const RouterConfig = () => {
+  return (
+    <Routes>
+      <Route exact path={'dupa'} component={null} />
+    </Routes>
+  );
+};
+
+export default RouterConfig;
