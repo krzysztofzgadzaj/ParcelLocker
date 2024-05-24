a69afcef8fc096398f1e1f4b45b317a42145acaf
Krzysztof Zgadzaj <kzgadzaj@future-processing.com>
2024-05-24 19:42:54 +0200

    .

---
+++ src/Modules/OutPost.Modules.Logistics.Domain/MediativeDeliveryPoints/MediativeDeliveryPointDrafts/MediativeDeliveryPointDraft.cs
@@ -0,0 +1,9 @@
+﻿using OutPost.Modules.Logistics.Domain.MediativeDeliveryPoints.MediativeDeliveryPointAccessors;
+using OutPost.Shared.Abstractions.Kernel.Types;
+
+namespace OutPost.Modules.Logistics.Domain.MediativeDeliveryPoints.MediativeDeliveryPointDrafts;
+
+public abstract class MediativeDeliveryPointDraft : AggregateRoot, IMdpDraftAvailability
+{
+    public abstract MediativeDeliveryPoint Activate();
+}
